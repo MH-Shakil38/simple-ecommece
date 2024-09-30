e85bb2fd51cd3e631b96d64cd7128275f6ad40f1
MH-Shakil38 <maynuddinhsn@gmail.com>
2024-09-30 18:05:07 +0600

    review

--- resources/views/website/version1/details.blade.php
+++ resources/views/website/version1/details.blade.php
@@ -67,7 +67,7 @@
                                     <div class="container">
                                         <span class="product-line hidden-phone hidden-v-tablet"></span>
                                         <span id="next-product" class="hidden-phone"><a
-                                                href="{{ route('single.product',$product->id+1) }}"
+                                                href="{{ route('single.product', $product->id + 1) }}"
                                                 rel="prev"><span></span><img fetchpriority="high" width="800"
                                                     height="800"
                                                     src="{{ asset('version1/') }}/wp-content/uploads/2023/06/Nutmegh-pelled.jpg"
@@ -76,7 +76,7 @@
                                                     srcset="{{ asset($product->image) }}"
                                                     sizes="(max-width: 800px) 100vw, 800px" /></a></span><span
                                             id="prev-product" class="hidden-phone"><a
-                                                href="{{ route('single.product',$product->id-1) }}"
+                                                href="{{ route('single.product', $product->id - 1) }}"
                                                 rel="next"><span></span><img width="1080" height="1080"
                                                     src="{{ asset('version1/') }}/wp-content/uploads/2023/08/4.png"
                                                     class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image"
@@ -320,16 +320,87 @@
                                             rel="tag">আচার</a></span>
                                 </div>
 
+                                <style>
+                                    .tab {
+                                        overflow: hidden;
+                                        border: 1px solid #ccc;
+                                        background-color: #f1f1f1;
+                                    }
+
+                                    /* Style the buttons inside the tab */
+                                    .tab button {
+                                        background-color: inherit;
+                                        float: left;
+                                        border: none;
+                                        outline: none;
+                                        cursor: pointer;
+                                        padding: 14px 16px;
+                                        transition: 0.3s;
+                                        font-size: 17px;
+                                    }
+
+                                    /* Change background color of buttons on hover */
+                                    .tab button:hover {
+                                        background-color: #ddd;
+                                    }
+
+                                    /* Create an active/current tablink class */
+                                    .tab button.active {
+                                        background-color: #ccc;
+                                    }
+
+                                    /* Style the tab content */
+                                    .tabcontent {
+                                        display: none;
+                                        padding: 6px 12px;
+                                        border: 1px solid #ccc;
+                                        border-top: none;
+                                    }
+                                </style>
+
+
+
+                                <script>
+                                    function openCity(evt, cityName) {
+                                        var i, tabcontent, tablinks;
+                                        tabcontent = document.getElementsByClassName("tabcontent");
+                                        for (i = 0; i < tabcontent.length; i++) {
+                                            tabcontent[i].style.display = "none";
+                                        }
+                                        tablinks = document.getElementsByClassName("tablinks");
+                                        for (i = 0; i < tablinks.length; i++) {
+                                            tablinks[i].className = tablinks[i].className.replace(" active", "");
+                                        }
+                                        document.getElementById(cityName).style.display = "block";
+                                        evt.currentTarget.className += " active";
+                                    }
+                                </script>
                                 <div class="details container">
-                                    <div class="title" >
-                                        <h4 style="border-bottom: 2px solid green;"> <span
+                                    <div class="title">
+                                        <div class="tab">
+                                            <button class="tablinks active"
+                                                onclick="openCity(event, 'description')">Description</button>
+                                            <button class="tablinks" onclick="openCity(event, 'review')">Review</button>
+                                        </div>
+                                        {{-- <h4 style="border-bottom: 2px solid green;"> <button
                                                 style="background: #168d44;
                                                         color: #fff;
                                                         font-size: large;
-                                                        padding: 5px;">Description</span>
-                                        </h4>
+                                                        padding: 5px;" onclick="openCity(event, 'description')">Description</button>
+                                        </h4> --}}
                                     </div>
-                                    <article style="font-size: 17px">{!! $product->description !!}</article>
+
+                                    <div id="description" class="tabcontent" style="display: block;">
+                                        <article style="font-size: 17px">{!! $product->description !!}</article>
+                                    </div>
+
+                                    <div id="review" class="tabcontent">
+                                        <h3>review</h3>
+                                        <p>review is the capital of France.</p>
+                                    </div>
+
+
+
 
                                 </div>
 
