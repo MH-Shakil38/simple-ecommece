921d0841fd29a802f7e47ff9b594ce29caf94b99
MH-Shakil38 <maynuddinhsn@gmail.com>
2024-10-05 14:21:15 +0600

    ..

--- resources/views/website/version1/checkout.blade.php
+++ resources/views/website/version1/checkout.blade.php
@@ -2103,45 +2103,8 @@
                 </div>
             </div>
         </div>
-        <footer class="footer-bottom dark">
-            <div class="wrap">
-
-                <div class="footer_content ">
-                    <div class="footer_content_left">
-                        <div class="copyright_logo">
-
-                            <div class="copyright">
-                                Copyright 2016 - 2024 Chuijhal.com , All Right Reserved </div>
-                        </div>
-                    </div>
-                    <div class="footer_content_right">
-
-                        <ul class="social-icons">
-                            <li class="socialLinkShortcode textstyle facebook">
-                                <a href="https://www.facebook.com/chuijhal.cooking/" target="_blank">
-                                    <span>Facebook</span>
-                                </a>
-                            </li>
-                            <li class="socialLinkShortcode textstyle youtube">
-                                <a href="https://www.youtube.com/channel/UChjwisbZJ9Yz-JRCR9NGqyg" target="_blank">
-                                    <span>YouTube</span>
-                                </a>
-                            </li>
-                            <li class="socialLinkShortcode textstyle pinterest">
-                                <a href="https://www.pinterest.com/chuijhalbd/" target="_blank">
-                                    <span>Pinterest</span>
-                                </a>
-                            </li>
-                            <li class="socialLinkShortcode textstyle instagram">
-                                <a href="https://www.instagram.com/chuijhal.cooking/" target="_blank">
-                                    <span>Instagram</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        @include('website.version1.layouts.include.footer')
+     
 
     </div>
     <div id="ep_wc_notices">
