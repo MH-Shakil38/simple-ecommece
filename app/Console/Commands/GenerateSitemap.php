2c17ec0a876ae1e3bdace3e99d21c2af42787458
MH-Shakil38 <maynuddinhsn@gmail.com>
2024-12-02 15:36:53 +0600

    ...

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap for the application';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully!');
+    }
+}
