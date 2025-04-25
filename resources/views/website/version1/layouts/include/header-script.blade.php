a9c5d8ac8e9d1bc7ba5e8b97ab8c5f16eb177b0e
MH-Shakil38 <maynuddinhsn@gmail.com>
2025-04-26 02:10:04 +0600

    ...

--- resources/views/website/version1/layouts/include/header-script.blade.php
+++ resources/views/website/version1/layouts/include/header-script.blade.php
@@ -1448,7 +1448,6 @@
         }
     </style>
 </noscript>
-{{-- {{---<meta name="facebook-domain-verification" content="r2a6d7bfatqpc6bo6j8w3pwh96ozkb" />---}} --}}
 <script type="text/javascript" src="{{ asset('version1/') }}/wp-includes/js/jquery/jquery.minf43b.js?ver=3.7.1"
     id="jquery-core-js"></script>
 <script type="text/javascript" src="{{ asset('version1/') }}/wp-includes/js/jquery/jquery-migrate.min5589.js?ver=3.4.1"
